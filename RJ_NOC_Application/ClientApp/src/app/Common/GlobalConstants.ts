43a3b1667be30afb5a48918dc7d3957b2a4be5fe
Rishi Kapoor <rishi.kapoor@corpnet.co.in>
2023-07-05 17:45:51 +0530

    updated

---
+++ RJ_NOC_Application/ClientApp/src/app/Common/GlobalConstants.ts
@@ -0,0 +1,17 @@
+export class GlobalConstants {
+  //public static apiURL: string = "http://localhost:62778/api/";
+ public static apiURL: string = "https://unocapi.devitsandbox.com/api/";
+  public static ApplicationTitle: string = "RJ_NOC";
+}
+
+
+//export class GlobalFuncation {
+//  public Encrypt(planTest: string) {
+//    
+//    return CryptoJS.AES.encrypt(planTest.trim(), "RJNoc465717444").toString();
+//  }
+//  public Decrypt(encryptText: string) {
+//    return CryptoJS.AES.decrypt(encryptText.trim(), "RJNoc465717444").toString(CryptoJS.enc.Utf8);
+//  }
+//} 
+
