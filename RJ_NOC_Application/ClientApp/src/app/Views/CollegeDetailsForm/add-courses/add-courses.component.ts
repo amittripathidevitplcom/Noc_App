fccee4c72ca4b59c7ae5fc3f1be1248ec1ba0c60
Rishi Kapoor <rishi.kapoor@corpnet.co.in>
2023-08-03 18:26:10 +0530

    update

--- RJ_NOC_Application/ClientApp/src/app/Views/CollegeDetailsForm/add-courses/add-courses.component.ts
+++ RJ_NOC_Application/ClientApp/src/app/Views/CollegeDetailsForm/add-courses/add-courses.component.ts
@@ -117,8 +117,6 @@ export class AddCoursesComponent implements OnInit {
     }
   }
   get form() { return this.CourseMasterForm.controls; }
-
-
   async LoadMaster() {
     try {
       this.loaderService.requestStarted();
@@ -138,8 +136,6 @@ export class AddCoursesComponent implements OnInit {
       }, 200);
     }
   }
-
-
   async GetStreamList() {
 
     try {
@@ -168,12 +164,6 @@ export class AddCoursesComponent implements OnInit {
     }
 
   }
-
-
-
-
-
-
   async ddlCollege_change(SeletedCollegeID: any) {
     this.request.CollegeID = SeletedCollegeID;
     await this.commonMasterService.GetCollegeBasicDetails(SeletedCollegeID)
@@ -268,9 +258,6 @@ export class AddCoursesComponent implements OnInit {
       }, 200);
     }
   }
-
-
-
   async GetAllList() {
     try {
       this.loaderService.requestStarted();
@@ -605,7 +592,6 @@ export class AddCoursesComponent implements OnInit {
       }, 200);
     }
   }
-
   async CourseLevel() {
     try {
       this.loaderService.requestStarted();
@@ -627,7 +613,6 @@ export class AddCoursesComponent implements OnInit {
       }, 200);
     }
   };
-
   async GetStreamList_CourseIDWise() {
     try {
 
@@ -649,7 +634,6 @@ export class AddCoursesComponent implements OnInit {
       }, 200);
     }
   }
-
   async ddlCourseLevel_change(CourseLevelID: any) {
 
     if (this.request.DepartmentID == 3) {
@@ -666,7 +650,6 @@ export class AddCoursesComponent implements OnInit {
       this.courseDataList = this.courseDataList.filter(item => item.CourseLevelID == CourseLevelID);
     }
   }
-
   showhideStream() {
     if (this.request.DepartmentID == EnumDepartment.CollegeEducation) {
       this.isShowStreambox = true;
@@ -675,7 +658,6 @@ export class AddCoursesComponent implements OnInit {
       this.isShowStreambox = false;
     }
   }
-
   async ddlStream_change(StreamID: any) {
     this.request.StreamID = StreamID;
     await this.commonMasterService.GetSubjectList_StreamIDWise(this.request.StreamID, this.request.DepartmentID, this.request.CourseLevelID, this.request.CourseID)
@@ -686,7 +668,6 @@ export class AddCoursesComponent implements OnInit {
         console.log(this.request.SelectedSubjectDetails);
       }, error => console.error(error));
   }
-
   async ddlSreamChangeReset(StreamID: any) {
     this.request.StreamID = StreamID;
     this.request.CourseLevelID = 0;
