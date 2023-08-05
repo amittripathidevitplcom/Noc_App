2dc215cb3554715d6fbd0c78a8122ef4f350b467
Naresh rawat <Naresh.rawat@corpnet.co.in>
2023-08-05 11:05:38 +0530

    Class Wise Student Changes

--- RJ_NOC_Application/ClientApp/src/app/Views/ClassWiseStudentDetails/class-wise-student-details/class-wise-student-details.component.ts
+++ RJ_NOC_Application/ClientApp/src/app/Views/ClassWiseStudentDetails/class-wise-student-details/class-wise-student-details.component.ts
@@ -4,7 +4,7 @@ import { LoaderService } from '../../../Services/Loader/loader.service';
 import { ClassWiseStudentDetailsServiceService } from '../../../Services/ClassWiseStudentDetails/class-wise-student-details-service.service';
 import { SSOLoginDataModel } from '../../../Models/SSOLoginDataModel';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { PostClassWiseStudentDetailsDataModel } from '../../../Models/ClassWiseStudentDetailsDataModel';
+import { ClassWiseStudentDetailsDataModel, PostClassWiseStudentDetailsDataModel } from '../../../Models/ClassWiseStudentDetailsDataModel';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
@@ -15,7 +15,10 @@ import { ToastrService } from 'ngx-toastr';
 export class ClassWiseStudentDetailsComponent implements OnInit {
 
 
+  //public ClassWiseStudentDetailsList: any = [];
+
   public ClassWiseStudentDetailsList: any = [];
+
   sSOLoginDataModel = new SSOLoginDataModel();
   public SelectedCollageID: number = 0;
   public SelectedDepartmentID: number = 0;
@@ -92,6 +95,30 @@ export class ClassWiseStudentDetailsComponent implements OnInit {
 
       }, 200);
     }
+  }
+  CalculateAll(item: any,index :any) {
+
+    //boys section
+    let vSCB = item.SCBoysCount == undefined ? 0 : item.SCBoysCount;
+    let vSTB = item.STBoysCount == undefined ? 0 : item.STBoysCount;
+    let vOBC = item.OBCBoysCount == undefined ? 0 : item.OBCBoysCount;
+    let vMBCB = item.MBCBoysCount == undefined ? 0 : item.MBCBoysCount;
+    let vGENB = item.GenBoysCount == undefined ? 0 : item.GenBoysCount;
+    let vEWSB = item.EWSBoysCount == undefined ? 0 : item.EWSBoysCount;
+
+    //girls
+    let vSCG = item.SCGirlsCount == undefined ? 0 : item.SCGirlsCount;
+    let vSTG = item.STGirlsCount == undefined ? 0 : item.STGirlsCount;
+    let vOBG = item.OBCGirlsCount == undefined ? 0 : item.OBCGirlsCount;
+    let vMBCG = item.MBCGirlsCount == undefined ? 0 : item.MBCGirlsCount;
+    let vGENG = item.GenGirlsCount == undefined ? 0 : item.GenGirlsCount;
+    let vEWSG = item.EWSGirlsCount == undefined ? 0 : item.EWSGirlsCount;
+
+
+    item.TotalBoys = Number(vSCB) + Number(vSTB) + Number(vOBC) + Number(vMBCB) + Number(vGENB) + Number(vEWSB);
+    item.TotalGirls = Number(vSCG) + Number(vSTG) + Number(vOBG) + Number(vMBCG) + Number(vGENG) + Number(vEWSG);
+
+   
   }
   //validattions
   numberOnly(event: any): boolean
