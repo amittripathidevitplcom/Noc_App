ee03077857463ee57c2f65e8857adb810b5b215f
Rishi Kapoor <rishi.kapoor@corpnet.co.in>
2023-09-26 14:29:25 +0530

    updated

--- RJ_NOC_Application/ClientApp/src/app/Views/ApplicationDetailEntry/application-detail-entry/application-detail-entry.component.ts
+++ RJ_NOC_Application/ClientApp/src/app/Views/ApplicationDetailEntry/application-detail-entry/application-detail-entry.component.ts
@@ -516,6 +516,22 @@ export class ApplicationDetailEntryComponent implements OnInit {
         this.IsShowDraftFinalSubmit = false;
       }
     }
+
+    //Tech Education
+    if (this.SelectedDepartmentID == 6) {
+      if (this.CollegeType_IsExisting == true) {
+        if (this.CheckTabsEntryData['LandInformation'] > 0 && this.CheckTabsEntryData['Facility'] > 0 && this.CheckTabsEntryData['RequiredDocument'] > 0 && this.CheckTabsEntryData['RoomDetails'] > 0 && this.CheckTabsEntryData['OtherInformation'] > 0 && this.CheckTabsEntryData['BuildingDocuments'] > 0 && this.CheckTabsEntryData['StaffDetails'] > 0 && this.CheckTabsEntryData['OLDNOCDetails'] > 0 && this.CheckTabsEntryData['AcademicInformation'] > 0)
+        {
+          this.IsShowDraftFinalSubmit = false;
+        }
+      }
+      else {
+        if (this.CheckTabsEntryData['LandInformation'] > 0 && this.CheckTabsEntryData['Facility'] > 0 && this.CheckTabsEntryData['RequiredDocument'] > 0 && this.CheckTabsEntryData['RoomDetails'] > 0 && this.CheckTabsEntryData['OtherInformation'] > 0 && this.CheckTabsEntryData['BuildingDocuments'] > 0 &&  this.CheckTabsEntryData['HostelDetails'] > 0) {
+          this.IsShowDraftFinalSubmit = false;
+        }
+      }
+    }
+
   }
 
 }
