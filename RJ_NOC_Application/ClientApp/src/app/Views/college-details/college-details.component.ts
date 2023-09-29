90952ecd21bf0f755ee185ab4d94fc9f073bfa89
Rishi Kapoor <rishi.kapoor@corpnet.co.in>
2023-09-29 18:31:25 +0530

    updated

--- RJ_NOC_Application/ClientApp/src/app/Views/college-details/college-details.component.ts
+++ RJ_NOC_Application/ClientApp/src/app/Views/college-details/college-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AutocompleteLibModule } from 'angular-ng-autocomplete';
 import { AutoComplete } from 'primeng/autocomplete';
 
@@ -11,20 +11,25 @@ import { AutoComplete } from 'primeng/autocomplete';
   styleUrls: ['./college-details.component.css']
 })
 export class CollegeDetailsComponent implements OnInit {
+  isSubmitted: boolean = false;
+  isDisabledGrid: boolean = false;
   //@ViewChild("search") searchElement!: ElementRef;
   //@ViewChild('autoCompleteObject') private autoCompleteObject!: AutoComplete;
-   
+
   @ViewChild('auto') auto: any;
   CourseMasterForm!: FormGroup;
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private el: ElementRef) { }
   ngOnInit(): void {
     this.CourseMasterForm = this.formBuilder.group(
       {
-        txtSearch: ['']
+        txtSearch: [''],
+        //txttest: ['', Validators.required]
+        txttest: [{ value: '', disabled: true }, Validators.required],
+        txttest2: [{ value: '', disabled: true }, Validators.required],
       })
   }
 
-
+  get form() { return this.CourseMasterForm.controls; }
 
 
   keyword = 'name';
@@ -66,18 +71,33 @@ export class CollegeDetailsComponent implements OnInit {
 
 
 
- 
+
 
   show() {
+    this.isSubmitted = true;
+    debugger;
+    const invalidElements = this.el.nativeElement.querySelectorAll('.ng-invalid');
+    if (invalidElements.length > 0) {
+      console.log(invalidElements[0]);
+      invalidElements[1].focus();
+    }
+
+    if (this.CourseMasterForm.invalid) {
+      return
+    }
     console.log(this.auto);
     this.auto.focus();
-    //setTimeout(() => {
-    //  // this will make the execution after the above boolean has changed
-    //  this.searchElement.nativeElement.focus();
-    //}, 0);
+  }
+  checkf() {
+    console.log("ab");
+    //this.CourseMasterForm.get('txttest')?.enable();
+    this.CourseMasterForm.controls['txttest'].enable();
+    this.CourseMasterForm.controls['txttest2'].enable();
 
+    
+    //this.isDisabledGrid = true;
+    //console.log(this.isDisabledGrid);
 
-
-    //this.autoCompleteObject.focusInput();
+    //this.isDisabledGrid = false;
   }
 }
