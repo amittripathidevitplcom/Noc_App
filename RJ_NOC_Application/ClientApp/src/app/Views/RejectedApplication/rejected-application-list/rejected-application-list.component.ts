4ece7923487d4927e86d46c8025171725036c194
Naresh rawat <Naresh.rawat@corpnet.co.in>
2023-11-02 19:26:41 +0530

    Update

---
+++ RJ_NOC_Application/ClientApp/src/app/Views/RejectedApplication/rejected-application-list/rejected-application-list.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit, Input, Injectable } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import * as XLSX from 'xlsx';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+import { CommonMasterService } from '../../../Services/CommonMaster/common-master.service';
+import { LoaderService } from '../../../Services/Loader/loader.service';
+import { SSOLoginDataModel } from '../../../Models/SSOLoginDataModel';
+import { CollegeService } from '../../../services/collegedetailsform/College/college.service';
+import { DCEDocumentScrutinyService } from '../../../Services/DCEDocumentScrutiny/dcedocument-scrutiny.service';
+import { ModalDismissReasons, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { CommitteeMasterService } from '../../../Services/Master/CommitteeMaster/committee-master.service';
+
+@Component({
+  selector: 'app-rejected-application-list',
+  templateUrl: './rejected-application-list.component.html',
+  styleUrls: ['./rejected-application-list.component.css']
+})
+export class RejectedApplicationListComponent {
+  //Add FormBuilder
+  ProjectMasterForm!: FormGroup;
+  public State: number = -1;
+  public SuccessMessage: any = [];
+  public ErrorMessage: any = [];
+  /*Save Data Model*/
+
+  public UserID: number = 0;
+  public RejectedApplicatoinListData: any = [];
+  searchText: string = '';
+  sSOLoginDataModel = new SSOLoginDataModel();
+  closeResult: string | undefined;
+  constructor(private committeeMasterService: CommitteeMasterService, private collegeservice: CollegeService, private toastr: ToastrService, private loaderService: LoaderService, private modalService: NgbModal,
+    private formBuilder: FormBuilder, private commonMasterService: CommonMasterService, private router: ActivatedRoute, private routers: Router, private _fb: FormBuilder, private decDocumentScrutinyService: DCEDocumentScrutinyService) {
+
+  }
+
+  async ngOnInit() {
+    this.sSOLoginDataModel = await JSON.parse(String(localStorage.getItem('SSOLoginUser')));
+    await this.RejectedApplicationList();
+  }
+
+  async RejectedApplicationList() {
+    try {
+      this.loaderService.requestStarted();
+      await this.collegeservice.RejectedApplicationList(this.sSOLoginDataModel.SSOID)
+        .then((data: any) => {
+          data = JSON.parse(JSON.stringify(data));
+          this.State = data['State'];
+          this.SuccessMessage = data['SuccessMessage'];
+          this.ErrorMessage = data['ErrorMessage'];
+          this.RejectedApplicatoinListData = data['Data'][0]['data'];
+        }, error => console.error(error));
+    }
+    catch (Ex) {
+      console.log(Ex);
+    }
+    finally {
+      setTimeout(() => {
+        this.loaderService.requestEnded();
+      }, 200);
+    }
+  }
+
+  async ApplicationSummary_OnClick(DepartmentID: number, CollegeID: number) {
+    this.routers.navigate(['/applicationsummary' + "/" + encodeURI(this.commonMasterService.Encrypt(DepartmentID.toString())) + "/" + encodeURI(this.commonMasterService.Encrypt(CollegeID.toString()))]);
+  }
+}
