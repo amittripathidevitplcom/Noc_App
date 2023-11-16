8dcf08415d2f1b906daa14d517d336aaa0ff95e1
Rishi Kapoor <rishi.kapoor@corpnet.co.in>
2023-11-16 16:15:07 +0530

    updated

--- RJ_NOC_Application/ClientApp/src/app/app.component.ts
+++ RJ_NOC_Application/ClientApp/src/app/app.component.ts
@@ -11,15 +11,15 @@ export class AppComponent {
   title = 'RJ-NOC';
   public configUrl: any = "";
   async ngOnInit() {
-    if (localStorage.getItem('apiurl') == null) {
+    if (sessionStorage.getItem('apiurl') == null) {
       this.configUrl = window['window'];
-      localStorage.setItem('apiurl', this.configUrl["config"]['apiurl']);
-      localStorage.setItem('backtossourl', this.configUrl["backtossourl"]['apiurl']);
-      localStorage.setItem('backtossourl_logout', this.configUrl["config"]['backtossourl_logout']);
-      localStorage.setItem('imagepathurl', this.configUrl["config"]['imagepathurl']);
-      localStorage.setItem('ssourl', this.configUrl["config"]['ssourl']);
-      localStorage.setItem('applicationtitle', this.configUrl["config"]['applicationtitle']);
-      localStorage.setItem('rpprequsturl', this.configUrl["config"]['rpprequsturl']);
+      sessionStorage.setItem('apiurl', this.configUrl["config"]['apiurl']);
+      sessionStorage.setItem('backtossourl', this.configUrl["backtossourl"]['apiurl']);
+      sessionStorage.setItem('backtossourl_logout', this.configUrl["config"]['backtossourl_logout']);
+      sessionStorage.setItem('imagepathurl', this.configUrl["config"]['imagepathurl']);
+      sessionStorage.setItem('ssourl', this.configUrl["config"]['ssourl']);
+      sessionStorage.setItem('applicationtitle', this.configUrl["config"]['applicationtitle']);
+      sessionStorage.setItem('rpprequsturl', this.configUrl["config"]['rpprequsturl']);
 
       GlobalConstants._ApiURL = this.configUrl["config"]['apiurl'];
       GlobalConstants._BacktoSSOURL = this.configUrl["backtossourl"]['apiurl'];
@@ -31,13 +31,13 @@ export class AppComponent {
 
     }
     else {
-      GlobalConstants._ApiURL = localStorage.getItem('apiurl')?.toString();
-      GlobalConstants._BacktoSSOURL = localStorage.getItem('backtossourl')?.toString();
-      GlobalConstants._BacktoSSOURL_Logout = localStorage.getItem('backtossourl_logout')?.toString();
-      GlobalConstants._ImagePathURL = localStorage.getItem('imagepathurl')?.toString();
-      GlobalConstants._SSOURL = localStorage.getItem('ssourl')?.toString();
-      GlobalConstants._ApplicationTitle = localStorage.getItem('applicationtitle')?.toString();
-      GlobalConstants._RPPRequstURL = localStorage.getItem('rpprequsturl')?.toString();
+      GlobalConstants._ApiURL = sessionStorage.getItem('apiurl')?.toString();
+      GlobalConstants._BacktoSSOURL = sessionStorage.getItem('backtossourl')?.toString();
+      GlobalConstants._BacktoSSOURL_Logout = sessionStorage.getItem('backtossourl_logout')?.toString();
+      GlobalConstants._ImagePathURL = sessionStorage.getItem('imagepathurl')?.toString();
+      GlobalConstants._SSOURL = sessionStorage.getItem('ssourl')?.toString();
+      GlobalConstants._ApplicationTitle = sessionStorage.getItem('applicationtitle')?.toString();
+      GlobalConstants._RPPRequstURL = sessionStorage.getItem('rpprequsturl')?.toString();
     }
   }
 }
