ad4e956cee1c4a80b53ac0a2b9283f98821efb76
Rishi Kapoor <rishi.kapoor@corpnet.co.in>
2023-12-27 16:06:31 +0530

    updated

---
+++ RJ_NOC_Application/ClientApp/src/app/Views/DocumentScrutinyTab_DTE/document-scrutiny-check-list-dte/document-scrutiny-check-list-dte.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-scrutiny-check-list-dte',
+  templateUrl: './document-scrutiny-check-list-dte.component.html',
+  styleUrls: ['./document-scrutiny-check-list-dte.component.css']
+})
+export class DocumentScrutinyCheckListDTEComponent {
+
+}
