58466c399a35573fec732c90961d589f19ae1acd
Rishi Kapoor <rishi.kapoor@corpnet.co.in>
2025-04-16 17:29:09 +0530

    Commitby Amit AddcorepolicyinProgramcsfile

--- RJ_NOC_Application/Program.cs
+++ RJ_NOC_Application/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddCors(x => x.AddPolicy("corepolicy", x1 => x1.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().WithExposedHeaders("X-AuthToken", "no-loader")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,6 +12,7 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseStaticFiles();
+app.UseCors("corepolicy");
 app.UseRouting();
 
 
